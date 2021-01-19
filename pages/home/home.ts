dfef388f77d852a5059825cafa51ece9ab63acf5
Group3Emtec2021 <tobistovillar@gmail.com>
2021-01-19 15:48:03 +0800

    2 files modified, 1 added

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -3,7 +3,8 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  styleUrls: ['home.css']
 })
 export class HomePage {
   feet: number;
@@ -38,7 +39,4 @@ export class HomePage {
     else{
     }
   }
-  
-  
-  
 }
